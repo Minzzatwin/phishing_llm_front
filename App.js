c16a41d715627bac29e4ffa5500071ee11648c8d
Minzzatwin <minsoojeon05@outlook.kr>
2025-08-30 16:07:53 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,55 +1,56 @@
 // App.js
-import * as React from 'react';
+import React from 'react';
+import  { useState } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import HomeScreen from './screens/Community/HomeScreen';
+import DetailScreen from './screens/Community/DetailScreen';
+import WriteScreen from './screens/Community/WriteScreen';
+import { PostProvider } from './store/PostContext';
+import TabNavigator from './navigation/TabNavigator';
+import styles from './styles';
+import { StatusBar } from 'react-native';
 
-import MainScreen from './screens/MainScreen';
-import LogScreen from './screens/LogScreen';
-import DetectScreen from './screens/DetectScreen';
-import CommunityScreen from './screens/CommunityScreen';
 
-//Stack Navigator 생성
-const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      {/*
-        Stack.Navigator는 스택 기반의 내비게이션 정의
-        새로운 화면이 스택 위에 쌓이고, 뒤로 가기 버튼을 누르면 스택에서 제거됨
-      */}
-      <Stack.Navigator initialRouteName="MainScreen">
-        {/*
-          Stack.Screen은 내비게이션 스택에 등록할 각 화면을 정의
-          'name'은 화면의 고유한 이름이고, 'component'는 해당 화면에 렌더링될 컴포넌트
-        */}
-        <Stack.Screen
-          name="MainScreen"
-          component={MainScreen}
-          // 메인 화면에서는 상단 내비게이션 헤더를 숨김
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="LogScreen"
-          component={LogScreen}
-          // '탐지 로그' 화면의 상단 헤더 제목을 설정
-          options={{ title: '탐지 로그' }}
-        />
-        <Stack.Screen
-          name="DetectScreen"
-          component={DetectScreen}
-          // '피싱 판별' 화면의 상단 헤더 제목을 설정
-          options={{ title: '피싱 판별' }}
-        />
-        <Stack.Screen
-          name="CommunityScreen"
-          component={CommunityScreen}
-          // '커뮤니티' 화면의 상단 헤더 제목을 설정
-          options={{ title: '커뮤니티' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <SafeAreaProvider>
+//       <PostProvider>  
+//         <NavigationContainer>
+//         <Stack.Navigator initialRouteName="Home">
+//           <Stack.Screen name="Home" component={HomeScreen} options={{ title: '홈' }} />
+//           <Stack.Screen name="Detail" component={DetailScreen} options={{ title: '글 상세' }} />
+//           <Stack.Screen name="Write" component={WriteScreen} options={{ title: '글 쓰기' }} />
+//         </Stack.Navigator>
+//         </NavigationContainer>      
+//       </PostProvider>
+//     </SafeAreaProvider>
+//   );
+// }
+
+export default function App(){
+  return(
+    
+    <SafeAreaProvider>
+      <StatusBar barStyle="dark-content" backgroundColor="#fafafa" />
+      <PostProvider>
+        <NavigationContainer>
+          <TabNavigator />
+        </NavigationContainer>    
+      </PostProvider>
+    </SafeAreaProvider>
   );
 }
-
-export default App;
